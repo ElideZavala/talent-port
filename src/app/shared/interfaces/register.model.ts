cc7b7831156b17c720cf0a41e1fc703c14b66039
Giovani Quezada <giovani.quezada@gmail.com>
2022-07-26 14:49:49 -0500

    cleanup

--- src/app/shared/interfaces/register.model.ts
+++
@@ -1,9 +0,0 @@
-export interface RegisterInterface {
-       name: string;
-      lastName: string;
-      phone: string;
-      email: string;
-      birthday: string | undefined;
-      password: string;
-      confirmPassword: string;
-}
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  API: 'http://get:5000/'
-
-};
