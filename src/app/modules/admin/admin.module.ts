6164b9eba252f84b2a7aba0bfd47dd35ea6fefae
ElideZavala <zavalavinagreelide@gmail.com>
2022-08-03 11:57:18 -0500

    se agrega proceso del account-edit

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -1,23 +1,27 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { SharedModule } from 'src/app/shared/shared.module';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { CommonModule, DatePipe } from '@angular/common';
+
 import { InvitationCodeComponent } from './invitation-code/invitation-code.component';
-import { AccountEditComponent } from './account-edit/account-edit.component';
+import { AccountEditComponent } from './account-edit/components/account-edit.component';
 import { AdminRoutingModule } from './admin-fronted-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ExampleHeader } from './account-edit/components/account-edit.component'
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 
 @NgModule({
   declarations: [
     InvitationCodeComponent,
-    AccountEditComponent
+    AccountEditComponent,
+    ExampleHeader
   ],
   imports: [
     CommonModule,
     SharedModule,
     AdminRoutingModule,
-
-
-  ]
+    ReactiveFormsModule
+  ],
+  providers: [DatePipe, { provide: LOCALE_ID, useValue: 'es-MX' }],
 })
 export class AdminModule { }
